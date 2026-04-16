06b8db529c6e59e51dd893b0821d17c236218b6c
testmkm0515 <testmkm0515@naver.com>
2026-04-16 14:31:30 +0900

    초기 프로젝트 upload

---
+++ spring_boot_jpa_bookEx/src/main/java/com/spring_boot_jpa_book/projectEx/SpringBootJpaBookExApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_jpa_book.projectEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaBookExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaBookExApplication.class, args);
+	}
+
+}
